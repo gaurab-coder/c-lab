233395fd9857cf0532c6adb2428f9c72315eea55
gaurab-coder <gaurabkhadka332@gmail.com>
2025-06-24 18:52:25 +0545

    c labs
    armstrong number

---
+++ armstrong.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{ 
+int n,num,r,arm=0;
+printf("Enter the number");
+scanf("%d", &n);
+num=n;
+while(num!=0)
+{
+r=num%10;
+arm=arm+r*r*r;
+num=num/10;
+}
+if(n==arm)
+printf("%d is armstrong number",n);
+else
+printf("%d is not armstrong number",n);
+getch();
+}
