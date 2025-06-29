7e8a3041cd56ea2d1155d5de4f9baa8c9ed157a6
gaurab-coder <gaurabkhadka332@gmail.com>
2025-06-29 20:42:49 +0545

    c lab

---
+++ assignment operator.c
@@ -0,0 +1,17 @@
+//Assignment Operator
+#include <stdio.h>
+int main()
+{
+int x = 100;
+printf("Gaurab Khadka\n Roll no. 13\n");
+printf("Value of x is %d\n", x); //100
+x += 100;
+printf("Value of x is %d\n", x); //200
+x -= 100;
+printf("Value of x is %d\n", x); //100
+x *= 100;
+printf("Value of x is %d\n", x); //10000
+x /= 100;
+printf("Value of x is %d\n", x); //100
+return 0;
+}
