9f970d7da2c3e896d2c258f64478d3bc7db992c5
gaurab-coder <gaurabkhadka332@gmail.com>
2025-07-03 19:17:14 +0545

    c lab

---
+++ stars pattern.c
@@ -0,0 +1,14 @@
+// Program to print the pattern of the stars
+
+#include<stdio.h>
+int main(){
+	int i, j;
+printf("Gaurab khadka Roll no:13\n");
+	 for(i=5; i>0; i--){
+	 	for(j=0; j<i; j++){
+	 		printf("*");	
+		 }
+		 printf("\n");
+	 }
+	 return 0;
+}
