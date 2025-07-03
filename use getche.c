9244f882e9117be25bd00a1061a6d5fa1aab9142
gaurab-coder <gaurabkhadka332@gmail.com>
2025-07-03 19:18:27 +0545

    c lab

---
+++ use getche.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+   char ch1, ch2;
+   printf("Gaurab khadka Roll no:13\n");
+   
+   printf("Enter first character:\n");
+   ch1=getch();
+   
+   printf("Enter 2nd character:\n");
+   ch2=getch();
+   
+   printf("1st character:\n");
+   putch(ch1);
+   
+   printf("2nd character:\n");
+   putch(ch2);
+   
+   return 0;
+}
+
